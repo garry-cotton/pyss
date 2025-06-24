d4d20895befc94ccd4aa641b782be15e5f0a1e70
cosmicbhejafry <47458458+cosmicbhejafry@users.noreply.github.com>
2025-06-24 15:24:11 +1000

    deleted:    jupyter_notebooks/synthetic_data_simulations/sklearn_synthetic_data_eda.ipynb 	modified:   pyspoc/rstatistics/test.py 	renamed:    jupyter_notebooks/pyspoc_trials/check_imports_nb.ipynb -> working_analyses/pyspoc_trial_and_setup/check_imports_nb.ipynb 	renamed:    jupyter_notebooks/pyspoc_trials/mymodule.py -> working_analyses/pyspoc_trial_and_setup/mymodule.py 	renamed:    jupyter_notebooks/pyspoc_trials/trial_add_custom_stat.ipynb -> working_analyses/pyspoc_trial_and_setup/trial_add_custom_stat.ipynb 	renamed:    jupyter_notebooks/synthetic_data_simulations/blob_distCurve_metrics.ipynb -> working_analyses/synthetic_data_simulations/blob_distCurve_metrics.ipynb 	new file:   working_analyses/synthetic_data_simulations/sklearn_synthetic_data_eda.ipynb

--- pyspoc/rstatistics/test.py
+++ pyspoc/rstatistics/test.py
@@ -8,3 +8,34 @@ class ReducedCovarianceTest(Covariance, ReducedStatistic):
     def compute(self, data: np.ndarray):
         result = Covariance.compute(self, data)        
         return result.diagonal().flatten()
+    
+class PCABase(ReducedStatistic, ABC):
+
+    def __init__(self, components: list[int]):
+        self.__cached_pca = None
+        self._components = components
+        self.__n_components = max(self._components)
+    
+    def calculate(self, dataset: Dataset):
+        cached_pca_tuple = self.__cached_pcas.get(dataset)
+        cached_pca = None
+        curr_n_components = 0
+
+        if cached_pca_tuple:
+            cached_pca, curr_n_components = cached_pca_tuple
+
+        if curr_n_components >= self.__n_components:
+            self.__cached_pca = cached_pca
+
+        result = super().calculate(dataset)
+        self.__cached_pcas[dataset] = (self.__cached_pca, self.__n_components)
+        return result
+    
+    def _get_pca(self, data: np.ndarray) -> PCA:
+        if self.__cached_pca is not None:
+            return self.__cached_pca
+        
+        pca = PCA(n_components=self.__n_components)
+        pca.fit(data)
+        self.__cached_pca = pca
+        return pca
